46709e385b9cb45b25b4b9c651f764eb2f724ba0
Jörg Herzinger <joerg.herzinger@oiml.at>
2011-08-30 10:09:18 +0300

    fix bug with git attributes if file is not existing yet

--- bin/LVInit.sh
+++ bin/LVInit.sh
@@ -18,16 +18,16 @@ function do_git_config {
 case "$OPT" in
 	--system)
 		GIT_CONFIG_OPTS="--system"
-		ATTRIBUTES_FILE="/etc/gitattributes"
+		ATTRIBUTES_FILE=/etc/gitattributes
 	;;
 	--global)
 		GIT_CONFIG_OPTS="--global"
-		ATTRIBUTES_FILE="~/.gitattributes"
+		ATTRIBUTES_FILE=~/.gitattributes
 	;;
 	--local)
 		git status &> /dev/null || { echo "You are not in a GIT Repository"; exit 0; }
 		GIT_CONFIG_OPTS="--local"
-		ATTRIBUTES_FILE=".git/info/attributes"
+		ATTRIBUTES_FILE=.git/info/attributes
 	;;
 	*) 
 		echo -e "Usage: \"$0 option\" where option can be one of the following
@@ -42,6 +42,6 @@ esac
 do_git_config "git config ${GIT_CONFIG_OPTS}"
 
 # Create attributes file if missing and write specifics
-ATTRIBUTES_FILE_ABSOLUTE=$(eval ls ${ATTRIBUTES_FILE})
+ATTRIBUTES_FILE_ABSOLUTE=$(echo ${ATTRIBUTES_FILE})
 touch ${ATTRIBUTES_FILE_ABSOLUTE}
 echo -e "*.vi diff=labview mergetool=labview merge=labview\n" >> ${ATTRIBUTES_FILE_ABSOLUTE}
